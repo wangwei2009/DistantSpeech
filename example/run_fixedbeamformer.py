2482bd6350fe29f24cb162b890274a1ad5260324
wangwei <wwei@eliteai.com.cn>
2019-06-24 14:53:43 +0800

    add beamformer test file

---
+++ example/run_fixedbeamformer.py
@@ -0,0 +1,65 @@
+"""
+Test beamformer
+
+"""
+
+import numpy as np
+
+import time
+
+from beamformer.MicArray import MicArray
+from beamformer.fixedbeamformer import fixedbeamfomer
+from beamformer.adaptivebeamformer import adaptivebeamfomer
+from beamformer.utils import mesh,pmesh,load_wav,load_pcm
+
+import matplotlib.pyplot as plt
+import sounddevice as sd
+import soundfile as sf
+from scipy.io import wavfile
+
+filepath = "E:/work/matlab/Github/beamformer/sound/rec1/"
+# filepath = "E:/work/kws/mycroft-latest/DistantSpeech/unit_test/pcm/"
+x,sr = load_wav(filepath)
+sr = 16000
+r = 0.032
+c = 343
+
+frameLen = 256
+hop = frameLen/2
+overlap = frameLen - hop
+nfft = 256
+c = 340
+r = 0.032
+fs = sr
+
+start = time.clock()
+
+MicArray = MicArray(arrayType='circular', r=0.032, M=4)
+angle = np.array([197, 0]) / 180 * np.pi
+
+fixedbeamformer = fixedbeamfomer(MicArray,frameLen,hop,nfft,c,r,fs)
+# """
+# fixed beamformer precesing function
+# method:
+# 'DS':   delay-and-sum beamformer
+# 'MVDR': MVDR beamformer under isotropic noise field
+#
+# """
+yout = fixedbeamformer.process(x,angle,method='MVDR',retH=True,retWNG=True,retDI=True)
+
+end = time.clock()
+print(end-start)
+
+# listen processed result
+sd.default.channels = 1
+sd.play(yout['data'],fs)
+sd.wait()
+
+# view beampattern
+mesh(yout['beampattern'])
+# pmesh(yout['beampattern'])
+
+# save audio
+# wavfile.write('output/output_fixedbeamformer.wav',16000,yout['data'])
+
+
