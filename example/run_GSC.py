e72897e4d7f751e815aa66a975556afc761c751c
wangwei <wwei@eliteai.com.cn>
2019-08-14 17:51:21 +0800

    add frequency-domain GSC algorithm,test local file OK

---
+++ example/run_GSC.py
@@ -0,0 +1,56 @@
+"""
+Test beamformer
+
+"""
+
+import numpy as np
+
+import time
+
+from beamformer.MicArray import MicArray
+from beamformer.GSC import GSC
+from beamformer.utils import mesh,pmesh,load_wav,load_pcm
+
+import matplotlib.pyplot as plt
+import sounddevice as sd
+import soundfile as sf
+from scipy.io import wavfile
+
+filepath = "test_audio/rec1/"
+x,sr = load_wav(filepath)
+sr = 16000
+r = 0.032
+c = 343
+
+frameLen = 256
+hop = frameLen/2
+overlap = frameLen - hop
+nfft = 256
+c = 340
+r = 0.032
+fs = sr
+
+start = time.clock()
+
+MicArray = MicArray(arrayType='circular', r=0.032, M=4)
+angle = np.array([197, 0]) / 180 * np.pi
+
+GSC_1 = GSC(MicArray,frameLen,hop,nfft,c,r,fs)
+yout = GSC_1.process(x,angle,method=3)
+
+end = time.clock()
+print(end-start)
+
+# listen processed result
+sd.default.channels = 1
+sd.play(yout['data'],fs)
+sd.wait()
+
+# view beampattern
+# mesh(yout['beampattern'])
+# pmesh(yout['beampattern'])
+
+# save audio
+# wavfile.write('output/output_fixedbeamformer.wav',16000,yout['data'])
+
+
