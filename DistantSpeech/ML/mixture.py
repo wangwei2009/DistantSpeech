c6e199f5ef49521e6a48eda792e451457a762790
wangwei <wwei@eliteai.com.cn>
2021-03-29 17:11:36 +0800

    1.EM done, need to handle numerical problem

--- DistantSpeech/ML/mixture.py
+++ DistantSpeech/ML/mixture.py
@@ -2,29 +2,38 @@ import argparse
 import os
 
 import numpy as np
-
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from sklearn import mixture
 
 
 class Gaussian(object):
     def __init__(self, feature=4):
         self.feature = feature
-        self.mean = np.zeros(feature)
-        self.var = np.random.rand(feature, feature)
+        self.mean = np.random.rand(feature)
+        self.var = np.eye(feature)
 
     def get_prob(self, x, diag=1e-6):
         p = self.get_log_prob(x, diag=diag)
-        # print(p)
         return np.exp(p)
-        # return p
 
     def get_log_prob(self, x, diag=1e-6):
+        """compute log prob per sample
 
-        assert  x.shape[0] == self.feature
+        Args:
+            x (ndarray): one sample of observations, [n_feature,] or [1, n_feature]
+            diag ([type], optional): [diagnal loading]. Defaults to 1e-6.
+
+        Returns:
+            [type]: [description]
+        """
+
         if len(x.shape) == 1:
-            x = x[:, np.newaxis]
-        mean = self.mean[:, np.newaxis]
+            x = x.reshape(-1, len(x))                  # in case of single sample
+        assert  x.shape[1] == self.feature
+        mean = self.mean[np.newaxis, :]
         var_ = self.var + np.eye((self.feature))*diag
-        p = -0.5*(self.feature*np.log(2*np.pi) + np.log(np.linalg.det(var_)) + ((x-mean).transpose() @ np.linalg.inv(var_) @ (x-mean)))
+        p = -0.5*(self.feature*np.log(2*np.pi) + np.log(np.linalg.det(var_)) + ((x-mean) @ np.linalg.inv(var_) @ (x-mean).transpose()))
 
         return p
 
@@ -45,7 +54,7 @@ class Gaussian(object):
 
         prob = 0.0
         for n in range(x.shape[0]):
-            prob = prob + self.get_prob(x[n, :])
+            prob = prob + self.get_prob(x[n:n+1, :])
 
         return prob
 
@@ -72,7 +81,21 @@ class GaussianMixture(object):
     def get_prob(self, x):
         prob = 0.0
         for z in range(self.n_components):
-            prob = prob + self.weights_[z] * self.gmms[z].get_log_prob(x)
+            prob = prob + self.weights_[z] * self.gmms[z].get_prob(x)
+        return prob
+
+    def get_log_prob(self, x):
+        prob = 0.0
+        for z in range(self.n_components):
+            prob = prob + np.exp(np.log(self.weights_[z]) + self.gmms[z].get_log_prob(x))
+        # prob = np.maximum(prob)
+        return np.log(prob)
+
+    def score_samples(self, x):
+        prob = np.zeros((x.shape[0],))
+        for n in range(x.shape[0]):
+            # print("x[{}, :]={}".format(n, x[n, :]))
+            prob[n] = self.get_log_prob(x[n, :])
         return prob
 
     def get_params(self):
@@ -88,6 +111,8 @@ class GaussianMixture(object):
         Args:
             x (ndarray): observation, [n_samples, n_features]
         """
+        if len(x.shape) == 1:
+            x = x[np.newaxis, :]               # for one sample
         assert x.shape[1] == self.n_features
         n_samples, n_feature = x.shape
         gamma = np.zeros((n_samples, self.n_components))
@@ -95,7 +120,7 @@ class GaussianMixture(object):
             prob_den = self.get_prob(x[n, :])
             # print(prob_den)
             for z in range(self.n_components):
-                gamma[n, z] = self.weights_[z] * self.gmms[z].get_log_prob(x[n,:])/prob_den
+                gamma[n, z] = self.weights_[z] * self.gmms[z].get_prob(x[n,:])/prob_den
 
         return gamma
 
@@ -123,35 +148,70 @@ class GaussianMixture(object):
 
     def update(self, x):
 
+        # E step
         gamma = self.compute_posterior(x)
+
+        # M step
         self.update_params(gamma, x)
 
-        prob = 0.0
-        for n in range(x.shape[0]):
-            prob = self.get_prob(x[n, :])
+        return np.sum(self.score_samples(x))
 
-        return prob
+def test_gaussain(shifted_gaussian, iter = 5):
+
+    # test gaussian
+    gaussian = Gaussian(feature=2)
+    for i in range(iter):
+        prob = gaussian.update(shifted_gaussian)
+        print("prob {}: {}".format(i, prob[0]))
+    print(gaussian.mean)
+
+    x = np.arange(-100., 100.,1)
+    y = np.arange(-100., 100.,1)
+    X, Y = np.meshgrid(x, y)
+    XX = np.array([X.ravel(), Y.ravel()]).T    # [40000, 2]
+    prob = 0.0
+    for i in range(len(y)):
+        for j in range(len(y)):
+            prob = prob + gaussian.get_prob(XX[i+j*len(y),:])
+    print("cdf {}".format(prob))                                 # integration of pdf should be equal 1
 
 
-def main(args):
-    n_features = 2
-    n_components = 2
-    gmm = GaussianMixture(n_components, n_features)
-    x = np.ones((n_features,1))
-    p = gmm.get_log_prob(x)
-    print(p)
+def test_gmm(X_train, iter = 5):
 
+    gmm = GaussianMixture(n_components=2, n_features=2)
 
+    for i in range(iter):
+        prob = gmm.update(X_train)
+        print("prob {}: {}".format(i, prob))
+
+    mean, var = gmm.get_params()
+    print("mean:{}".format(mean))
+    print("covariance:{}".format(var))
+
+    # display predicted scores by the model as a contour plot
+    x = np.linspace(-20., 30.)
+    y = np.linspace(-20., 40.)
+    X, Y = np.meshgrid(x, y)
+    XX = np.array([X.ravel(), Y.ravel()]).T
+    # Z = -clf.score_samples(XX)                        # score by sklearn
+    # Z = Z.reshape(X.shape)
+
+    Z = -gmm.score_samples(XX)
+    Z = Z.reshape(X.shape)
+
+    CS = plt.contour(X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0),
+                    levels=np.logspace(0, 3, 10))
+    CB = plt.colorbar(CS, shrink=0.8, extend='both')
+    plt.scatter(X_train[:, 0], X_train[:, 1], .8)
+
+    plt.title('Negative log-likelihood predicted by a GMM')
+    plt.axis('tight')
+    plt.show()
+    plt.savefig('pic2.png')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Neural Feature Extractor")
     args = parser.parse_args()
-    # main(args)
-
-    import numpy as np
-    import matplotlib.pyplot as plt
-    from matplotlib.colors import LogNorm
-    from sklearn import mixture
 
     n_samples = 300
 
@@ -171,38 +231,10 @@ if __name__ == "__main__":
     # fit a Gaussian Mixture Model with two components
     clf = mixture.GaussianMixture(n_components=2, covariance_type='full')
     clf.fit(X_train)
+    print(clf.get_params())
+    print("mean : {}".format(clf.means_))                # estimated by sklearn
+    print("var : {}".format(clf.covariances_))
 
-    iter = 100
-    gaussian = Gaussian(feature=2)
-    for i in range(iter):
-        prob = gaussian.update(stretched_gaussian)
-        print("prob {}: {}".format(i, prob[0]))
-    print(gaussian.mean)
+    test_gaussain(stretched_gaussian)
+    test_gmm(X_train)
 
-    # gmm = GaussianMixture(n_components=2, n_features=2)
-
-    # for i in range(iter):
-    #     prob = gmm.update(X_train)
-    #     # print(prob.shape)
-    #     print("prob {}: {}".format(i, prob[0]))
-    
-    # mean, var = gmm.get_params()
-    # print(mean)
-
-    # display predicted scores by the model as a contour plot
-    x = np.linspace(-20., 30.)
-    y = np.linspace(-20., 40.)
-    X, Y = np.meshgrid(x, y)
-    XX = np.array([X.ravel(), Y.ravel()]).T
-    Z = -clf.score_samples(XX)
-    Z = Z.reshape(X.shape)
-
-    CS = plt.contour(X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0),
-                    levels=np.logspace(0, 3, 10))
-    CB = plt.colorbar(CS, shrink=0.8, extend='both')
-    plt.scatter(X_train[:, 0], X_train[:, 1], .8)
-
-    plt.title('Negative log-likelihood predicted by a GMM')
-    plt.axis('tight')
-    plt.show()
-    plt.savefig('pic.png')
