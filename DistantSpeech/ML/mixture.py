c9916e73f67d9858571723694bc2bad7de94a2e1
wangwei <wangwei_2009@126.com>
2022-03-20 20:05:07 +0800

    1. nlacl formatting

--- DistantSpeech/ML/mixture.py
+++ DistantSpeech/ML/mixture.py
@@ -29,11 +29,11 @@ class Gaussian(object):
         """
 
         if len(x.shape) == 1:
-            x = x.reshape(-1, len(x))                  # in case of single sample
-        assert  x.shape[1] == self.feature
+            x = x.reshape(-1, len(x))  # in case of single sample
+        assert x.shape[1] == self.feature
         mean = self.mean[np.newaxis, :]
-        var_ = self.var + np.eye((self.feature))*diag
-        p = -0.5*(self.feature*np.log(2*np.pi) + np.log(np.linalg.det(var_)) + ((x-mean) @ np.linalg.inv(var_) @ (x-mean).transpose()))
+        var_ = self.var + np.eye((self.feature)) * diag
+        p = -0.5 * (self.feature * np.log(2 * np.pi) + np.log(np.linalg.det(var_)) + ((x - mean) @ np.linalg.inv(var_) @ (x - mean).transpose()))
 
         return p
 
@@ -48,13 +48,13 @@ class Gaussian(object):
 
         mean = self.mean[np.newaxis, :]
         for n in range(x.shape[0]):
-            xn = x[n:n+1, :]
-            self.var = self.var + ((xn-mean).transpose() @ (xn-mean))
-        self.var = self.var/n_sample
+            xn = x[n : n + 1, :]
+            self.var = self.var + ((xn - mean).transpose() @ (xn - mean))
+        self.var = self.var / n_sample
 
         prob = 0.0
         for n in range(x.shape[0]):
-            prob = prob + self.get_prob(x[n:n+1, :])
+            prob = prob + self.get_prob(x[n : n + 1, :])
 
         return prob
 
@@ -64,6 +64,7 @@ class Gaussian(object):
     def set_parm(self, mean, var):
         self.mean = mean
         self.var = var
+
 
 class GaussianMixture(object):
     def __init__(self, n_components=2, n_features=2) -> None:
@@ -112,7 +113,7 @@ class GaussianMixture(object):
             x (ndarray): observation, [n_samples, n_features]
         """
         if len(x.shape) == 1:
-            x = x[np.newaxis, :]               # for one sample
+            x = x[np.newaxis, :]  # for one sample
         assert x.shape[1] == self.n_features
         n_samples, n_feature = x.shape
         gamma = np.zeros((n_samples, self.n_components))
@@ -120,7 +121,7 @@ class GaussianMixture(object):
             prob_den = self.get_prob(x[n, :])
             # print(prob_den)
             for z in range(self.n_components):
-                gamma[n, z] = self.weights_[z] * self.gmms[z].get_prob(x[n,:])/prob_den
+                gamma[n, z] = self.weights_[z] * self.gmms[z].get_prob(x[n, :]) / prob_den
 
         return gamma
 
@@ -134,13 +135,12 @@ class GaussianMixture(object):
         N_z = np.sum(gamma, axis=0)
         n_samples = x.shape[0]
         for z in range(self.n_components):
-            self.mean_[z, :] = np.sum(gamma[:, z:z+1] * x, axis=0)/N_z[z]
+            self.mean_[z, :] = np.sum(gamma[:, z : z + 1] * x, axis=0) / N_z[z]
             for n in range(n_samples):
-                self.covar_[z, :, :] = self.covar_[z, :, :] + gamma[n, z] * ((x[n:n+1, :] - self.mean_[z:z+1, :]).transpose() @ (x[n:n+1, :] - self.mean_[z:z+1, :]))
-            self.covar_[z, :, :] = self.covar_[z, :, :]/N_z[z]
-            self.weights_[z] = N_z[z]/n_samples
+                self.covar_[z, :, :] = self.covar_[z, :, :] + gamma[n, z] * ((x[n : n + 1, :] - self.mean_[z : z + 1, :]).transpose() @ (x[n : n + 1, :] - self.mean_[z : z + 1, :]))
+            self.covar_[z, :, :] = self.covar_[z, :, :] / N_z[z]
+            self.weights_[z] = N_z[z] / n_samples
         self.set_params()
-
 
     def set_params(self):
         for z in range(self.n_components):
@@ -156,7 +156,8 @@ class GaussianMixture(object):
 
         return np.sum(self.score_samples(x))
 
-def test_gaussain(shifted_gaussian, iter = 5):
+
+def test_gaussain(shifted_gaussian, iter=5):
 
     # test gaussian
     gaussian = Gaussian(feature=2)
@@ -165,18 +166,18 @@ def test_gaussain(shifted_gaussian, iter = 5):
         print("prob {}: {}".format(i, prob[0]))
     print(gaussian.mean)
 
-    x = np.arange(-100., 100.,1)
-    y = np.arange(-100., 100.,1)
+    x = np.arange(-100.0, 100.0, 1)
+    y = np.arange(-100.0, 100.0, 1)
     X, Y = np.meshgrid(x, y)
-    XX = np.array([X.ravel(), Y.ravel()]).T    # [40000, 2]
+    XX = np.array([X.ravel(), Y.ravel()]).T  # [40000, 2]
     prob = 0.0
     for i in range(len(y)):
         for j in range(len(y)):
-            prob = prob + gaussian.get_prob(XX[i+j*len(y),:])
-    print("cdf {}".format(prob))                                 # integration of pdf should be equal 1
+            prob = prob + gaussian.get_prob(XX[i + j * len(y), :])
+    print("cdf {}".format(prob))  # integration of pdf should be equal 1
 
 
-def test_gmm(X_train, iter = 5):
+def test_gmm(X_train, iter=5):
 
     gmm = GaussianMixture(n_components=2, n_features=2)
 
@@ -189,8 +190,8 @@ def test_gmm(X_train, iter = 5):
     print("covariance:{}".format(var))
 
     # display predicted scores by the model as a contour plot
-    x = np.linspace(-20., 30.)
-    y = np.linspace(-20., 40.)
+    x = np.linspace(-20.0, 30.0)
+    y = np.linspace(-20.0, 40.0)
     X, Y = np.meshgrid(x, y)
     XX = np.array([X.ravel(), Y.ravel()]).T
     # Z = -clf.score_samples(XX)                        # score by sklearn
@@ -199,15 +200,15 @@ def test_gmm(X_train, iter = 5):
     Z = -gmm.score_samples(XX)
     Z = Z.reshape(X.shape)
 
-    CS = plt.contour(X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0),
-                    levels=np.logspace(0, 3, 10))
+    CS = plt.contour(X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0), levels=np.logspace(0, 3, 10))
     CB = plt.colorbar(CS, shrink=0.8, extend='both')
-    plt.scatter(X_train[:, 0], X_train[:, 1], .8)
+    plt.scatter(X_train[:, 0], X_train[:, 1], 0.8)
 
     plt.title('Negative log-likelihood predicted by a GMM')
     plt.axis('tight')
     plt.show()
     plt.savefig('pic2.png')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Neural Feature Extractor")
@@ -222,7 +223,7 @@ if __name__ == "__main__":
     shifted_gaussian = np.random.randn(n_samples, 2) + np.array([20, 20])
 
     # generate zero centered stretched Gaussian data
-    C = np.array([[0., -0.7], [3.5, .7]])
+    C = np.array([[0.0, -0.7], [3.5, 0.7]])
     stretched_gaussian = np.dot(np.random.randn(n_samples, 2), C)
 
     # concatenate the two datasets into the final training set
@@ -232,9 +233,8 @@ if __name__ == "__main__":
     clf = mixture.GaussianMixture(n_components=2, covariance_type='full')
     clf.fit(X_train)
     print(clf.get_params())
-    print("mean : {}".format(clf.means_))                # estimated by sklearn
+    print("mean : {}".format(clf.means_))  # estimated by sklearn
     print("var : {}".format(clf.covariances_))
 
     test_gaussain(stretched_gaussian)
     test_gmm(X_train)
-
