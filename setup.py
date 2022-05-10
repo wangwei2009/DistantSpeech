3a1781ac35fb83eb364ad984011d405311996232
wangwei <wwei@eliteai.com.cn>
2022-05-10 15:27:02 +0800

    1. add setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,34 @@
-#! /usr/bin/env python
+from setuptools import setup, find_packages
+import string
 
-# from distutils.core import setup, Extension
-# import numpy
-# import os
-
-# os.environ['CC'] = 'g++';
-# setup(name='matt_simple_test', version='1.0', ext_modules =[Extension('_mcra',
-#  ['mcra.c', 'mcra.i'], include_dirs = [numpy.get_include(),'.'])])
+# Load description
+def find_long_description():
+    with open('README.md', 'r') as f:
+        return f.read()
 
 
+# Read the version string
+def find_version():
+    version_var_name = '__version__'
+    with open('DistantSpeech/_version.py', 'r') as f:
+        for l in f:
+            if not l.startswith(version_var_name):
+                continue
+            return l[len(version_var_name) :].strip(string.whitespace + '\'"=')
+        raise RuntimeError('Unable to read version string.')
 
-# System imports
-from distutils.core import *
-from distutils      import sysconfig
 
-# Third-party modules - we depend on numpy for everything
-import numpy
-
-# Obtain the numpy include directory.  This logic works across numpy versions.
-try:
-    numpy_include = numpy.get_include()
-except AttributeError:
-    numpy_include = numpy.get_numpy_include()
-
-# ezrange extension module
-_mcra = Extension("_mcra",
-                   ["mcra.i","mcra.c"],
-                   include_dirs = [numpy_include],
-                   )
-
-# ezrange setup
-setup(  name        = "range function",
-        description = "range takes an integer and returns an n element int array where each element is equal to its index",
-        author      = "Egor Zindy",
-        version     = "1.0",
-        ext_modules = [_mcra]
-        )
+setup(
+    name='DistantSpeech',
+    # version=find_version(),
+    version='0.1.0',
+    description='A python package for distant speech enhancemnet.',
+    author='Wei Wang',
+    packages=[
+        "DistantSpeech",
+    ],
+    # packages=find_packages(exclude=('docs',)),
+    python_requires='>=3.5',
+    install_requires=['numpy>=1.14.0', 'scipy>=1.1.0', 'matplotlib>=2.1.0', 'librosa'],
+    zip_safe=False,
+)
