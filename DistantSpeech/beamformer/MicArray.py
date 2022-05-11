fd18986c027f760221d95f6ecd7d6876fd6b17d9
wangwei <wwei@eliteai.com.cn>
2022-05-11 11:25:54 +0800

    1. update mic array construction

--- DistantSpeech/beamformer/MicArray.py
+++ DistantSpeech/beamformer/MicArray.py
@@ -2,11 +2,25 @@ from scipy.signal import windows
 from scipy import signal
 import numpy as np
 import warnings
+
 from DistantSpeech.beamformer.ArraySim import ArraySim
 
 
-class MicArray(object):
+def cart2sph(x, y, z):
+    azimuth = np.arctan2(y, x)
+    elevation = np.arctan2(z, np.sqrt(x**2 + y**2))
+    r = np.sqrt(x**2 + y**2 + z**2)
+    return azimuth, elevation, r
 
+
+def sph2cart(azimuth, elevation, r):
+    x = r * np.cos(elevation) * np.cos(azimuth)
+    y = r * np.cos(elevation) * np.sin(azimuth)
+    z = r * np.sin(elevation)
+    return x, y, z
+
+
+class MicArray(object):
     def __init__(self, arrayType='circular', r=0.032, c=343, M=4, n_fft=256):
         self.arrayType = arrayType
         self.c = c
@@ -19,3 +33,43 @@ class MicArray(object):
         self.gamma = np.arange(0, 360, int(360 / self.M)) * np.pi / 180
 
         self.array_sim = ArraySim(arrayType, spacing=r, M=M)
+
+        self.array_type = arrayType
+        self.mic_loc = np.zeros((M, 3))
+        self.mic_loc = self.array_init()
+
+        """
+        # %                   ^ +z
+        # %                   |
+        # %                   |
+        # %                   |
+        # %                   |
+        # %                    --------------------> 90 degree
+        # %                  /                    +y
+        # %                 /
+        # %                /
+        # %               / +x
+        # %              0 degree
+        # % for linear arrays, mic0 lines on -y axis
+        # % 
+        # % for circular arrays, mic0 lies on +x axis
+        """
+
+    def array_init(self, mic_loc=None):
+        if self.array_type == 'circular':
+            az = np.arange(0, 360, int(360 / self.M)) * np.pi / 180  # azimuth of each mic
+            elevation = 0
+            for m in range(self.M):
+                self.mic_loc[m, :] = sph2cart(az[m], elevation, self.r)
+        elif self.array_type == 'linear':
+            self.mic_loc[:, 1] = (np.arange(self.M) - (self.M - 1) / 2) * self.r
+        else:
+            assert self.mic_loc.shape == mic_loc.shape, 'user defined mic location should be 2-D array with shape M X 3'
+            self.mic_loc = mic_loc
+
+        return self.mic_loc
+
+
+if __name__ == "__main__":
+    mic_array = MicArray(arrayType='circular', M=8)
+    print(mic_array.mic_loc)
