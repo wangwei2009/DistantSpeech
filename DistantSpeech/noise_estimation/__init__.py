47a70f6ac0166149e069dd22adfdd3b10d27aa02
wangwei <wwei@eliteai.com.cn>
2022-05-26 15:55:19 +0800

    1. add noise_esimation init file to simplify package import

---
+++ DistantSpeech/noise_estimation/__init__.py
@@ -0,0 +1,4 @@
+from DistantSpeech.noise_estimation.mcra import NoiseEstimationMCRA
+from DistantSpeech.noise_estimation.mcra2 import MCRA2
+from DistantSpeech.noise_estimation.mcspp_base import McSppBase
+from DistantSpeech.noise_estimation.mcspp import McSpp
