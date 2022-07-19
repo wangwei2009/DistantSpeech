3cc3265f21debc077123370983d05106434bff17
wangwei <wwei@eliteai.com.cn>
2022-07-19 11:17:51 +0800

    fix Fvv error when i==j

--- DistantSpeech/beamformer/gen_noise_msc.py
+++ DistantSpeech/beamformer/gen_noise_msc.py
@@ -4,7 +4,15 @@ from matplotlib import pyplot as plt
 from DistantSpeech.beamformer.MicArray import MicArray
 
 
-def gen_noise_msc(mic=MicArray, M=4, r=0.032, nfft=256, fs=16000, c=340):
+def gen_noise_msc(
+    mic: MicArray,
+    M=4,
+    r=0.032,
+    nfft=256,
+    fs=16000,
+    c=340,
+    Fvv_max=0.9998,
+):
     half_bin = round(nfft / 2 + 1)
     Fvv = np.zeros((half_bin, M, M))
     f = np.linspace(0, fs / 2, half_bin)
@@ -14,7 +22,7 @@ def gen_noise_msc(mic=MicArray, M=4, r=0.032, nfft=256, fs=16000, c=340):
         for j in range(0, M):
             dij = np.sqrt(np.sum((mic.mic_loc[i, :] - mic.mic_loc[j, :]) ** 2))
             if i == j:
-                Fvv[0, i, j] = 0.998
+                Fvv[:, i, j] = Fvv_max
             else:
                 Fvv[:, i, j] = np.sin(2 * np.pi * f * dij / c) / (2 * np.pi * f * dij / c)
 
@@ -29,5 +37,5 @@ if __name__ == "__main__":
     distance = np.linalg.norm(mic_array.mic_loc[i, :] - mic_array.mic_loc[j, :])
     plt.plot(gamma[:, i, j])
     plt.show()
-    plt.title('ideal diffuse noise field between mic{} and mic{}\n(distance:{} cm)'.format(j, j, distance))
+    plt.title('ideal diffuse noise field between mic{} and mic{}\n(distance:{} cm)'.format(i, j, distance))
     plt.savefig('gamma.png')
