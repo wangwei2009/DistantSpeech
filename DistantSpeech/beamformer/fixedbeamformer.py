3f7bbebe14ddde6d767a01dc736eb3e2b438ff63
wangwei <wwei@eliteai.com.cn>
2022-07-22 13:45:05 +0800

    rewrite fixedbeamformer

--- DistantSpeech/beamformer/fixedbeamformer.py
+++ DistantSpeech/beamformer/fixedbeamformer.py
@@ -1,94 +1,125 @@
 from scipy.signal import windows
 from scipy import signal
 import numpy as np
-from .MicArray import MicArray
-from .beamformer import beamformer
+from DistantSpeech.beamformer.MicArray import MicArray, compute_tau
+from DistantSpeech.beamformer.beamformer import beamformer
+from DistantSpeech.beamformer.gen_noise_msc import gen_noise_msc
+from DistantSpeech.beamformer.beamformer import compute_mvdr_weight
 import warnings
 
 
 class FixedBeamformer(beamformer):
-    def __init__(self, MicArray, frameLen=256, hop=None, nfft=None, c=343, fs=16000):
+    def __init__(self, MicArray, frameLen=256, hop=None, nfft=None, c=343, fs=16000, r=0.032):
 
         beamformer.__init__(self, MicArray, frame_len=frameLen, hop=hop, nfft=nfft, c=c, fs=fs)
         self.angle = np.array([197, 0]) / 180 * np.pi
-        self.gamma = MicArray.gamma
-        self.window = windows.hann(self.frameLen, sym=False)
-        self.win_scale = np.sqrt(1.0 / self.window.sum() ** 2)
-        self.freq_bin = np.linspace(0, self.half_bin - 1, self.half_bin)
-        self.omega = 2 * np.pi * self.freq_bin * self.fs / self.nfft
 
-        self.H = np.ones([self.M, self.half_bin], dtype=complex)
-
-        self.angle = np.array([0, 0]) / 180 * np.pi
+        self.angle = (197, 0)
 
         self.AlgorithmList = ["src", "DS", "MVDR"]
         self.AlgorithmIndex = 0
 
-    def process(self, x, angle, method=1, retH=False, retWNG=False, retDI=False):
+        self.W = self.compute_weights(look_angle=self.angle)
+
+    def compute_weights(
+        self,
+        look_angle=[90, 0],
+        weightType="SD",
+        diag_value=1e-3,
+    ):
+        """compute beamformer weights
+
+        Parameters
+        ----------
+        look_angle : list or tuple, optional
+            look angle in degree, by default [90, 0]
+        weightType : str, optional
+            fixedbeamformer type in ['DS', 'SD'], by default "DS"
+        diag_value : float, optional
+            diag value, by default 1e-6
+
+        Returns
+        -------
+        w: complex weights, [bins, M]
+            fixedbeamformer weights
         """
-        fixed beamformer precesing function
-        :param x: input data, [channels, samples]
-        :param angle: incident angle, [-pi, pi]
-        :param method:
-        :param retH:
-        :param retWNG:
-        :param retDI:
-        :return:
+
+        look_angle_rad = np.array(look_angle) / 180 * np.pi
+
+        a0 = self.compute_steering_vector_from_doa(look_angle=look_angle)
+
+        if weightType == 'DS':
+            W = a0 / self.M
+
+        if weightType == 'SD':
+            Fvv = gen_noise_msc(mic=self.MicArray)
+            diag = np.eye(self.M) * diag_value
+            diag_bin = np.broadcast_to(diag, (self.half_bin, self.M, self.M))
+            W = compute_mvdr_weight(a0, np.linalg.inv(Fvv + diag_bin))
+
+        return W
+
+    def process_freframe(self, X_n):
+        """processe single frame in subband
+           override this function if you have different process pipline
+
+        Parameters
+        ----------
+        X_n : np.array, complex array, [bins, C]
+            multichannel frequency frame
+
+        Returns
+        -------
+        Yf : np.array, [bins,]
+            processed frame
         """
+
+        # apply weights
+        Yf = np.einsum('ij, ij->i', self.W.conj(), X_n)
+
+        return Yf
+
+    def process(self, x, angle=(0, 0)):
+        """process core function for fixedbeamformer
+
+        Parameters
+        ----------
+        x : np.array, [samples, channel]
+            time-domain multichannel input chunk signal
+
+        Returns
+        -------
+        output : np.array, [samples, ]
+            enhanced time-domain single channel signal
         """
-        method:
-        'DS':   delay-and-sum beamformer
-        'MVDR': MVDR beamformer under isotropic noise field
-        """
-        X = self.transformer.stft(x.transpose())
-        frameNum = X.shape[1]
 
-        tao = -1 * self.r * np.cos(angle[1]) * np.cos(angle[0] - self.gamma) / self.c
+        assert x.shape[1] >= 2
 
-        if retWNG:
-            WNG = np.ones(self.half_bin)
-        else:
-            WNG = None
-        if retDI:
-            DI = np.ones(self.half_bin)
-        else:
-            DI = None
-        if retH is False:
-            beampattern = None
+        angle = list(angle)
 
-        # Fvv = gen_noise_msc(M, self.nfft, self.fs, self.r)
-        # H = np.mat(np.ones([self.half_bin, self.M]), dtype=complex).T
-        if (all(angle == self.angle) is False) or (method != self.AlgorithmIndex):
-            if method != self.AlgorithmIndex:
-                self.AlgorithmIndex = method
-            for k in range(0, self.half_bin):
-                a = np.mat(np.exp(-1j * self.omega[k] * tao)).T  # propagation vector
-                self.H[:, k, np.newaxis] = self.getweights(
-                    a, self.AlgorithmList[method], self.Fvv[k, :, :], Diagonal=1e-1
-                )
+        # re-calculate weights
+        if angle != self.angle:
+            # look_angle_rad = np.array(angle) / 180 * np.pi
+            self.W = self.compute_weights(look_angle=angle)
 
-                if retWNG:
-                    WNG[k] = self.calcWNG(a, self.H[:, k, np.newaxis])
-                if retDI:
-                    DI[k] = self.calcDI(a, self.H[:, k, np.newaxis], self.Fvv[k, :, :])
+        D = self.transform.stft(x)
 
-        Y = np.ones([self.half_bin, frameNum], dtype=complex)
-        for t in range(0, frameNum):
-            Z = X[:, t, :].transpose()
+        half_bin, frameNum, channel = D.shape
 
-            x_fft = np.array(np.conj(self.H)) * Z
-            Y[:, t] = np.sum(x_fft, axis=0)
+        # enhanced single channel spectrum
+        Yf = np.zeros((half_bin, frameNum, 1), dtype=complex)
 
-        yout = self.transformer.istft(Y)
+        # frame online processing block
+        for n in range(frameNum):
+            X_n = D[:, n, :]
 
-        # update angle
-        self.angle = angle
+            Yf[:, n, 0] = self.process_freframe(X_n)
 
-        # calculate beampattern
-        if retH:
-            beampattern = self.beampattern(self.omega, self.H)
+        output = self.transform.istft(Yf)
 
-        return {"data": yout, "WNG": WNG, "DI": DI, "beampattern": beampattern}
+        assert output.shape[0] == frameNum * self.hop, 'output:{}, x:{}'.format(output.shape, x.shape)
+
+        return output.squeeze()
 
 
 if __name__ == "__main__":
@@ -103,13 +134,17 @@ if __name__ == "__main__":
     c = 340
     r = 0.032
     fs = sr
+    M = 4
 
-    MicArrayObj = MicArray(arrayType="circular", r=0.032, M=4)
-    angle = np.array([197, 0]) / 180 * np.pi
+    MicArrayObj = MicArray(arrayType="circular", r=0.032, M=M)
+    angle = [197, 0]
 
     fixedbeamformerObj = FixedBeamformer(MicArrayObj, frameLen, hop, nfft, c, fs)
 
-    x = np.random.rand(16000 * 5)
+    W = fixedbeamformerObj.compute_weights(angle, weightType='DS')
+    W1 = fixedbeamformerObj.compute_weights(angle, weightType='SD')
+
+    x = np.random.rand(16000 * 5, M)
 
     # """
     # fixed beamformer precesing function
@@ -118,4 +153,10 @@ if __name__ == "__main__":
     # 'MVDR': MVDR beamformer under isotropic noise field
     #
     # """
-    yout = fixedbeamformerObj.process(x, angle, method=2, retH=True, retWNG=True, retDI=True)
+    yout = fixedbeamformerObj.process(x, angle)
+
+    beampattern = fixedbeamformerObj.compute_beampattern(MicArrayObj, weights=W.T)
+    beampattern1 = fixedbeamformerObj.compute_beampattern(MicArrayObj, weights=W1.T)
+    from DistantSpeech.beamformer.utils import pmesh, mesh
+
+    mesh(beampattern[:, 2:-2].T)
