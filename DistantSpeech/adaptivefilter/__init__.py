eafa79c85547941a8264c5a053ead98f9c861563
wangwei <wwei@eliteai.com.cn>
2022-07-26 16:45:20 +0800

    add import

--- DistantSpeech/adaptivefilter/__init__.py
+++ DistantSpeech/adaptivefilter/__init__.py
@@ -2,3 +2,7 @@ from .BaseFilter import BaseFilter
 from .BlockLMS import BlockLms
 from .FastFreqLms import FastFreqLms
 from .SubbandLMS import SubbandLMS
+from .SubbandAF import SubbandAF
+from .BaseFilter import awgn
+from .mdf import Mdf
+from .SubbandRLS import SubbandRLS
