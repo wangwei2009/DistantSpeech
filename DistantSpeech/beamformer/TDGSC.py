c88f2701daebdb07e82d53c518cf66ad2233db02
wangwei <wwei@eliteai.com.cn>
2022-10-24 16:24:14 +0800

    add time domain gsc beamformer

---
+++ DistantSpeech/beamformer/TDGSC.py
@@ -0,0 +1,158 @@
+"""
+time domain GSC beamformer
+==============
+
+----------
+
+
+
+"""
+import numpy as np
+from DistantSpeech.noise_estimation.mcra import NoiseEstimationMCRA
+from DistantSpeech.noise_estimation.mcspp_base import McSppBase
+from DistantSpeech.noise_estimation.omlsa_multi import NsOmlsaMulti
+from DistantSpeech.noise_estimation.mc_mcra import McMcra
+from DistantSpeech.noise_estimation.mcspp import McSpp
+from DistantSpeech.transform.transform import Transform
+from DistantSpeech.beamformer.beamformer import beamformer
+from DistantSpeech.beamformer.fixedbeamformer import TimeAlignment
+from DistantSpeech.beamformer.MicArray import MicArray
+from DistantSpeech.adaptivefilter.FastFreqLms import FastFreqLms
+from DistantSpeech.adaptivefilter.feature import Emphasis, FilterDcNotch16
+
+
+class TDGSC(beamformer):
+    def __init__(
+        self,
+        mic_array: MicArray,
+        frameLen=256,
+        angle=[197, 0],
+    ):
+        beamformer.__init__(self, mic_array, frame_len=frameLen)
+        self.mic_array = mic_array
+
+        self.angle = np.array(angle) / 180 * np.pi if isinstance(angle, list) else angle
+
+        self.time_alignment = TimeAlignment(mic_array, angle=self.angle)
+        self.aic_filter = FastFreqLms(filter_len=frameLen, n_channels=self.M - 1, non_causal=True)
+        self.dc_notch_mic = []
+        for _ in range(self.M):
+            self.dc_notch_mic.append(FilterDcNotch16(radius=0.98))
+
+        self.mcra = NoiseEstimationMCRA(nfft=frameLen * 2)
+        self.transform = Transform(n_fft=frameLen * 2, hop_length=frameLen, channel=1)
+
+        self.spp = self.mcra
+
+    def fixed_beamformer(self, x):
+        """fixed beamformer
+
+        Parameters
+        ----------
+        x : np.array
+            input multichannel data, [samples, chs]
+
+        Returns
+        -------
+        np.array
+            output data, [samples, 1]
+        """
+
+        return np.mean(x, axis=1, keepdims=True)
+
+    def blocking_matrix(self, x):
+        """fixed blocking matrix
+
+        Parameters
+        ----------
+        x : np.array
+            input multichannel data, [samples, chs]
+
+        Returns
+        -------
+        bm_output : np.array
+            output data, [samples, chs-1]
+        """
+        samples, channels = x.shape
+        bm_output = np.zeros((samples, channels - 1))
+        for m in range(channels - 1):
+            bm_output[:, m] = x[:, m] - x[:, m + 1]
+
+        return bm_output
+
+    def aic(self, y_fbf, bm_output):
+        """adaptive interference cancellation block
+
+        Parameters
+        ----------
+        y_fbf : np.array
+            output from fixed beamformer, upper path, (n_samples, 1)
+        bm_output : np.array
+            output from blocking matrix output, lower path, (n_samples, n_chs)
+
+        Returns
+        -------
+        output_n : np.array
+            _description_
+        """
+        # AIC block
+        output_n, _ = self.aic_filter.update(bm_output, y_fbf, fir_truncate=30)
+
+        return output_n
+
+    def process(self, x):
+        """time domain GSC beamformer processing function
+
+        Parameters
+        ----------
+        x : np.array
+            input multichannel data, [samples, chs]
+
+        Returns
+        -------
+        output : np.array
+            output enhanced data, [samples, ]
+        """
+
+        samples, channels = x.shape
+        output = np.zeros(samples)
+
+        for m in range(channels):
+            x[:, m], self.dc_notch_mic[m].mem = self.dc_notch_mic[m].filter_dc_notch16(x[:, m])
+
+        D = self.transform.stft(x[:, 0])
+        print(D.shape)
+
+        # overlaps-save approach, no need to use hop_size
+        frameNum = int((samples) / self.frameLen)
+
+        p = np.zeros((self.spp.half_bin, frameNum))
+
+        for n in range(frameNum):
+            x_n = x[n * self.frameLen : (n + 1) * self.frameLen, :]
+
+            p[:, n] = self.spp.estimation(D[:, n, :])
+
+            x_aligned = self.time_alignment.process(x_n)
+
+            fixed_output = self.fixed_beamformer(x_aligned)
+
+            bm_output = self.blocking_matrix(x_aligned)
+
+            # AIC block
+            output_n, _ = self.aic_filter.update(bm_output, fixed_output, fir_truncate=30)
+
+            # output[n * self.frameLen : (n + 1) * self.frameLen] = fixed_output[:, 0]
+            # output[n * self.frameLen : (n + 1) * self.frameLen] = np.squeeze(bm_output[:, 0])
+            output[n * self.frameLen : (n + 1) * self.frameLen] = np.squeeze(output_n)
+
+        return output
+
+
+if __name__ == "__main__":
+
+    M = 4
+    mic_array = MicArray(arrayType="circular", r=0.032, M=M)
+    gsc_beamformer = TDGSC(mic_array)
+    x = np.random.randn(16000 * 5, 4)
+    output = gsc_beamformer.process(x)
