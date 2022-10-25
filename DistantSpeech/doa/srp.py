6922d784b46d05253cc68765f341742b22e6cf1f
wangwei <wwei@eliteai.com.cn>
2022-10-25 14:39:13 +0800

    add srp

--- DistantSpeech/doa/srp.py
+++ DistantSpeech/doa/srp.py
@@ -1,5 +1,5 @@
-from matplotlib.pyplot import angle_spectrum
-
+# from matplotlib.pyplot import angle_spectrum
+from tqdm import tqdm
 import numpy as np
 from DistantSpeech.beamformer.MicArray import MicArray
 from DistantSpeech.transform.transform import Transform
@@ -12,25 +12,34 @@ class srp(object):
         self.transform = Transform(channel=mic_array.M, n_fft=mic_array.n_fft, hop_length=int(mic_array.n_fft / 2))
 
     def compute_angle_spectrum(self, x, phat=True, resolution=1):
+        """steer response power
+
+        Parameters
+        ----------
+        x : np.array
+            multichannel input data, [samples, chs]
+        phat : bool, optional
+            phase-transform, by default True
+        resolution : int, optional
+            angle interval, by default 1
+
+        Returns
+        -------
+        angle_spectrum : np.array
+            angle spectrum, [360, half_bin]
+        """
         y = self.transform.stft(x)  # [bins, frames, M]
+        print(y.shape)
 
         n_frame = y.shape[1]
         angle_spectrum = np.zeros((360, n_frame))
-        for n in tqdm(range(n_frame)):
-            y_n = y[:, n, :]
-            for angle in np.arange(0, 360, resolution):
-                # for angle in range(360):
-                steer_vecotr = self.mic_array.steering_vector(look_direction=angle)  # [M, bin]
+        for angle in tqdm(np.arange(0, 360, resolution)):
+            steer_vecotr = self.mic_array.steering_vector(look_direction=angle)  # [bin, M]
+            for n in range(n_frame):
+                y_p = steer_vecotr.conj() * y[:, n, :]
                 if phat:
-                    angle_spectrum[angle : angle * resolution, n] = np.sum(
-                        np.abs(np.sum(steer_vecotr.conj().T * y_n, axis=-1) ** 2)
-                    )
-                else:
-                    angle_spectrum[angle : angle * resolution, n] = np.sum(
-                        np.abs(np.sum(steer_vecotr.conj().T * y_n / np.abs(y_n.T), axis=-1) ** 2)
-                    )
-
-        # np.einsum('ij,il->ijl', target_n, target_n.conj())
+                    y_p = y_p / np.abs(y_p)
+                angle_spectrum[angle : angle + resolution, n] = np.sum(np.abs(np.sum(y_p, axis=-1)))
 
         return angle_spectrum
 
@@ -46,14 +55,17 @@ if __name__ == "__main__":
     from DistantSpeech.beamformer.utils import pmesh
     from librosa import power_to_db
 
-    audio_path = '/home/wangwei/work/DistantSpeech/example/mix.wav'
-    audiodata = audioread(audio_path)
-    print(audiodata.shape)
+    array_data = []
+    for n in range(2, 6):
+        filename = '/home/wangwei/work/DistantSpeech/samples/audio_samples/xmos/rec1/音轨-{}.wav'.format(n)
+        data_ch = audioread(filename)
+        array_data.append(data_ch)
+    array_data = np.transpose(np.array(array_data))
+    print(array_data.shape)
 
-    mic_array = MicArray(arrayType='circular', r=0.05, M=6)
+    mic_array = MicArray(arrayType='circular', r=0.032, M=4, n_fft=512)
     srp_obj = srp(mic_array)
 
-    # srp_obj.show()
-    angle_spectrum = srp_obj.compute_angle_spectrum(audiodata, resolution=10)
+    angle_spectrum = srp_obj.compute_angle_spectrum(array_data, resolution=1)
     pmesh(power_to_db(angle_spectrum))
     plt.savefig('angle_spectrum.png')
